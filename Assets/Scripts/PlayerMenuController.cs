0ee7fce0b8b7ad977831d402a6dd2c2339b0803c
Tharle Josefi de Camargo <tharlejc@gmail.com>
2024-02-08 15:55:21 -0500

    Animations player menu

---
+++ Assets/Scripts/PlayerMenuController.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerMenuController : MonoBehaviour
+{
+    [SerializeField] private float m_Speed;
+    private Animator m_PlayerAnimator;
+    private Rigidbody2D m_Rigidbody;
+    private bool m_LookingRight;
+
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+        m_PlayerAnimator = GetComponentInChildren<Animator>();
+        m_LookingRight = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float axisHorizontal = Input.GetAxis("Horizontal");
+
+        FlipPlayer(axisHorizontal);
+
+        Vector3 velocity = transform.right * axisHorizontal * m_Speed;
+        m_Rigidbody.velocity = velocity;
+        m_PlayerAnimator.SetFloat("velocity", velocity.magnitude);
+    }
+
+    private void FlipPlayer(float axisHorizontal)
+    {
+        Vector3 scale = transform.localScale;
+        if (axisHorizontal < 0 && m_LookingRight) 
+        { 
+            m_LookingRight = false;
+            scale.x *= -1; // flip
+        }
+
+        if (axisHorizontal > 0 && !m_LookingRight)
+        {
+            m_LookingRight = true;
+            scale.x *= -1; // flip
+        }
+        transform.localScale = scale;
+    }
+}
